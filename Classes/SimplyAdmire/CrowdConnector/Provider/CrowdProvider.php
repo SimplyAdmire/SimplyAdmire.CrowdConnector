92750cfa752b6d626475461e8527f69283fafb85
Lars Nieuwenhuizen <lars@keenvantage.nl>
2015-09-08 15:51:06 +0200

    [WIP][TASK] Create http request to authenticate user

--- Classes/SimplyAdmire/CrowdConnector/Provider/CrowdProvider.php
+++ Classes/SimplyAdmire/CrowdConnector/Provider/CrowdProvider.php
@@ -1,6 +1,8 @@
 <?php
 namespace SimplyAdmire\CrowdConnector\Provider;
 
+use TYPO3\Flow\Http\Request;
+use TYPO3\Flow\Http\Uri;
 use TYPO3\Flow\Security\Account;
 use TYPO3\Flow\Security\Authentication\Provider\AbstractProvider;
 use TYPO3\Flow\Http\Client\CurlEngine as HttpClient;
@@ -20,7 +22,7 @@ class CrowdProvider extends AbstractProvider {
 	 * @Flow\InjectConfiguration(path="security.authentication.providers.CrowdProvider.providerOptions", package="TYPO3.Flow")
 	 * @var array
 	 */
-	protected $ldapOptions;
+	protected $providerOptions;
 
 	/**
 	 * @Flow\Inject
@@ -29,10 +31,30 @@ class CrowdProvider extends AbstractProvider {
 	protected $httpClient;
 
 	/**
+	 * @Flow\Inject
+	 * @var Request
+	 */
+	protected $request;
+
+	/**
 	 * @param TokenInterface $authenticationToken
 	 * @return void
 	 */
 	public function authenticate(TokenInterface $authenticationToken) {
+		$credentials = $authenticationToken->getCredentials();
+		if (is_array($credentials) && isset($credentials['username']) && isset($credentials['password'])) {
+			$providerName = $this->name;
+			$uri = new Uri($this->providerOptions['crowdServerUrl'] . $this->providerOptions['apiUrls']['authenticate']); // combined api url
+
+			/** @todo create a correct http request */
+			$request = $this->request->create($uri, 'GET', []); // building the request
+			$response = $this->httpClient->sendRequest($request); // sending the request
+			$result = json_decode($response->getContent()); // json result decoded
+
+			if (isset($result['name']) && $result['name'] === $credentials['username']) {
+				// go see if account exists :)
+			}
+		}
 	}
 
 	public function getTokenClassNames() {
